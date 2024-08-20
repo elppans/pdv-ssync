dc7c194a84475f9678caf8e6dc4f6405b4ea89eb
elppans <elppansmk@gmail.com>
2024-08-20 12:43:03 -0300

    modificado pdv_release.sh

--- pdv_release.sh
+++ pdv_release.sh
@@ -16,15 +16,15 @@ if [ ! -e ""$iponpdv"" ]; then
 fi
 
 # shellcheck disable=SC2013
-for IP in $(cat ""$iponpdv""); do
-        if ping -c 1 ""$IP"" >>/dev/null; then
-                echo -e """"$IP""" ON!"
-                # shellcheck disable=SC2154
-                # shellcheck disable=SC2140
-                sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" root@""$IP"" "cat /etc/canoalinux-release" ||
-                        sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" user@""$IP"" "cat /etc/canoalinux-release" ||
-                        sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" zanthus@""$IP"" "cat /etc/canoalinux-release"
-        else
-                echo -e """"$IP""" OFF!"
-        fi
+# shellcheck disable=SC2154
+# shellcheck disable=SC2140
+for IP in $(cat "$iponpdv"); do
+	if ping -c 1 "$IP" >>/dev/null; then
+		echo -e """$IP"" ON!"
+		sshpass -p "$senha_criptografada" ssh "$ssh_options" root@"$IP" "cat /etc/canoalinux-release" 2>/dev/null ||
+			sshpass -p "$senha_criptografada" ssh "$ssh_options" user@"$IP" "cat /etc/canoalinux-release" 2>/dev/null ||
+			sshpass -p "$senha_criptografada" ssh "$ssh_options" zanthus@"$IP" "cat /etc/canoalinux-release" 2>/dev/null
+	else
+		echo -e """$IP"" OFF!"
+	fi
 done
