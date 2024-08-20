ea08d440f56af1481add180af547ed2d88e00789
elppans <elppansmk@gmail.com>
2024-08-20 17:08:02 -0300

    modificado pdv_release.sh e pdv_keyscan.sh

--- pdv_release.sh
+++ pdv_release.sh
@@ -24,7 +24,7 @@ for IP in $(cat "$iponpdv"); do
 	if ping -c 1 "$IP" >>/dev/null; then
 		echo -e """$IP"" ON!"
                 # shellcheck disable=SC2154
-                "$pdvmod/ssh-keyscan.sh" """$IP""" >> /dev/null
+                "$pdvmod/ssh-keyscan.sh" """$IP""" &>>/dev/null
                 exit
 		sshpass -p "$senha_criptografada" ssh "$ssh_options" root@"$IP" "cat /etc/canoalinux-release" 2>>/dev/null ||
 			sshpass -p "$senha_criptografada" ssh "$ssh_options" user@"$IP" "cat /etc/canoalinux-release" 2>>/dev/null ||
