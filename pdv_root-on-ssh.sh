bdc8f0368d308c2bbf32a94bc9caba79c7fc0f22
elppans <elppansmk@gmail.com>
2024-08-21 13:01:30 -0300

    modificado pdv_root-on-ssh.sh

--- pdv_root-on-ssh.sh
+++ pdv_root-on-ssh.sh
@@ -5,10 +5,10 @@ source /usr/share/pdv-ssync/environment/pdv_env
 
 # shellcheck disable=SC2154
 if [[ -e "$pdvcripto" ]]; then
-	source "$pdvcripto"
+    source "$pdvcripto"
 else
-	echo "Falta arquivo de criptografia!"
-	exit 0
+    echo "Falta arquivo de criptografia!"
+    exit 0
 fi
 
 # shellcheck disable=SC2154
@@ -46,14 +46,20 @@ for IP in $(cat "$IPON"); do
         echo -e "\n$IP ON!"
         # shellcheck disable=SC2154
         "$pdvmod/ssh-keyscan.sh" "$IP" &>>/dev/null
+        execute_ssh_pdv16() {
+            execute_ssh_commands "$IP" "user" ""$senha_criptografada"" ""$ssh_options""
+        }
 
+        execute_ssh_pdv22() {
+            execute_ssh_commands "$IP" "zanthus" ""$senha_criptografada"" ""$ssh_options""
+        }
         # Verifica a versão do Ubuntu e executa os comandos apropriados
         if sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" user@"$IP" "lsb_release -r | grep -q '16.04'"; then
-            execute_ssh_commands "$IP" "user" ""$senha_criptografada"" ""$ssh_options""
+            execute_ssh_pdv16
             # echo "Ubuntu 16"
             # exit
         elif sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" zanthus@"$IP" "lsb_release -r | grep -q '22.04'"; then
-        execute_ssh_commands "$IP" "zanthus" ""$senha_criptografada"" ""$ssh_options""
+            execute_ssh_pdv22
         else
             echo "Não foi possível verificar o sistema do IP \"$IP\""
         fi
