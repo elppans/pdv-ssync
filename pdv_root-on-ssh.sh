21ffa842e22cda6744d48fb7c5f41d56c899f96d
elppans <elppansmk@gmail.com>
2024-08-21 15:19:37 -0300

    modificado pdv_release.sh

--- pdv_root-on-ssh.sh
+++ pdv_root-on-ssh.sh
@@ -39,9 +39,8 @@ for IP in $(cat "$IPON"); do
             export ssh_options
 
             sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" "$USER"@"$IP" "
-            echo \"$PASSWORD\" | sudo -S sed -i '/^PermitRootLogin prohibit-password/!b;/^#PermitRootLogin prohibit-password/b;s/^PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/' /etc/ssh/sshd_config; 
-            echo \"$PASSWORD\" | sudo -S sh -c 'grep -q \"^PermitRootLogin yes$\" /etc/ssh/sshd_config || echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config'; 
-            echo \"$PASSWORD\" | sudo -S systemctl restart sshd;
+            echo "OK 1"
+            echo "OK 2"
             " 2>>/dev/null
         }
         # Verifica a versão do Ubuntu e executa os comandos apropriados
