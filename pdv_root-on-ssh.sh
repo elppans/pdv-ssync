a6695b983e69f7b4cd3135dc02cd0fa1c669c15f
elppans <elppansmk@gmail.com>
2024-08-21 15:34:14 -0300

    modificado pdv_root-on-ssh.sh

--- pdv_root-on-ssh.sh
+++ pdv_root-on-ssh.sh
@@ -43,7 +43,7 @@ for IP in $(cat "$IPON"); do
             echo \"$PASSWORD\" | sudo -S sed -i '/^PermitRootLogin prohibit-password/!b;/^#PermitRootLogin prohibit-password/b;s/^PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/' /etc/ssh/sshd_config; 
             echo \"$PASSWORD\" | sudo -S sh -c 'grep -q \"^PermitRootLogin yes$\" /etc/ssh/sshd_config || echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config'; 
             echo \"$PASSWORD\" | sudo -S systemctl restart sshd;
-            grep PermitRootLogin /etc/ssh/sshd_config && echo "Login SSH OK" || echo "Login SSH não configurado"
+            grep -q PermitRootLogin /etc/ssh/sshd_config && echo "Login SSH OK" || echo "Login SSH não configurado"
             #systemctl status sshd
             " 2>>/dev/null
         }
