50f61a8736e663227ae3680bfa89cc90f5ee2136
elppans <elppansmk@gmail.com>
2024-08-21 16:02:22 -0300

    modificado pdv_descanso.sh

--- pdv_descanso.sh
+++ pdv_descanso.sh
@@ -5,10 +5,10 @@ source /usr/share/pdv-ssync/environment/pdv_env
 
 # shellcheck disable=SC2154
 if [[ -e "$pdvcripto" ]]; then
-	source "$pdvcripto"
+    source "$pdvcripto"
 else
-	echo "Falta arquivo de criptografia!"
-	exit 0
+    echo "Falta arquivo de criptografia!"
+    exit 0
 fi
 
 # shellcheck disable=SC2154
@@ -34,23 +34,23 @@ fi
 # shellcheck disable=SC2013
 for IP in $(cat "$iponpdv"); do
 
-# Função para sincronização do Descanso
-ssh_sync(){
-export senha_criptografada
-export rsync_options
-export ssh_options
-export descanso
-export IP
+    # Função para sincronização do Descanso
+    ssh_sync() {
+        export senha_criptografada
+        export rsync_options
+        export ssh_options
+        export descanso
+        export IP
 
-sshpass -p "$senha_criptografada" rsync ""$rsync_options"" ssh ""$ssh_options"" "$descanso"/ root@"$IP":"$guiconfigproj"/
-}
+        sshpass -p "$senha_criptografada" rsync ""$rsync_options"" "ssh "$ssh_options"" "$descanso"/ root@"$IP":"$guiconfigproj"/
+    }
 
-# Verifica se o IP está comunicando
-if ping -c 1 "$IP" >> /dev/null; then
-echo -e """$IP"" ON!"
-# Faz a sincronização via Função SSH
-ssh_sync
- else
-echo -e """$IP"" OFF!"
-fi
+    # Verifica se o IP está comunicando
+    if ping -c 1 "$IP" >>/dev/null; then
+        echo -e """$IP"" ON!"
+        # Faz a sincronização via Função SSH
+        ssh_sync
+    else
+        echo -e """$IP"" OFF!"
+    fi
 done
