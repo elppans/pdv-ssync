d9c720610c7f280597bc55f72355577a0483ca93
elppans <elppansmk@gmail.com>
2024-08-21 16:07:14 -0300

    modificado pdv_descanso.sh

--- pdv_descanso.sh
+++ pdv_descanso.sh
@@ -42,7 +42,9 @@ for IP in $(cat "$iponpdv"); do
         export descanso
         export IP
 
-        sshpass -p "$senha_criptografada" rsync ""$rsync_options"" ssh ""$ssh_options"" "$descanso"/ root@"$IP":"$guiconfigproj"/
+        sshpass -p ""$senha_criptografada"" \
+        rsync ""$rsync_options"" \
+        ssh ""$ssh_options"" "$descanso"/ root@"$IP":"$guiconfigproj"/
     }
 
     # Verifica se o IP está comunicando
