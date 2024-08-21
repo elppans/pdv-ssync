af16745728bb68129b3bb2405001247446ad73ea
elppans <elppansmk@gmail.com>
2024-08-21 15:41:53 -0300

    modificado pdv_root-on-ssh.sh

--- pdv_root-on-ssh.sh
+++ pdv_root-on-ssh.sh
@@ -40,10 +40,10 @@ for IP in $(cat "$IPON"); do
             export ssh_options
 
             sshpass -p ""$senha_criptografada"" ssh ""$ssh_options"" "$USER"@"$IP" "
-            echo \"$PASSWORD\" | sudo -S sed -i '/^PermitRootLogin prohibit-password/!b;/^#PermitRootLogin prohibit-password/b;s/^PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/' /etc/ssh/sshd_config; 
-            echo \"$PASSWORD\" | sudo -S sh -c 'grep -q \"^PermitRootLogin yes$\" /etc/ssh/sshd_config || echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config'; 
-            echo \"$PASSWORD\" | sudo -S systemctl restart sshd;
-            grep -q PermitRootLogin /etc/ssh/sshd_config && echo "Login SSH OK" || echo "Login SSH não configurado"
+            echo \"$PASSWORD\" | sudo -S sed -i '/^PermitRootLogin prohibit-password/!b;/^#PermitRootLogin prohibit-password/b;s/^PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/' /etc/ssh/sshd_config &>>/dev/null; 
+            echo \"$PASSWORD\" | sudo -S sh -c 'grep -q \"^PermitRootLogin yes$\" /etc/ssh/sshd_config || echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config' &>>/dev/null; 
+            echo \"$PASSWORD\" | sudo -S systemctl restart sshd &>>/dev/null;
+            grep -q PermitRootLogin /etc/ssh/sshd_config && echo -e "\nLogin SSH OK" || echo -e "\nLogin SSH não configurado"
             #systemctl status sshd
             " 2>>/dev/null
         }
